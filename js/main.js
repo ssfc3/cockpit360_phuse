c737126331326f226f1273dfcda98b52fa4cc615
Tessa Thornton <tessathornton@gmail.com>
2013-01-21 14:27:11 -0500

    add sticky header

--- js/main.js
+++ js/main.js
@@ -294,4 +294,36 @@ $(document).ready(function(){
         $(".assign-user").remove();
     });
 
+    var itemPos = $(".app-header").offset();
+    var headHeight = $(".app-header").css("height");
+
+    $(document).scroll(function(){
+        var scrollPos = $(document).scrollTop(); 
+        if (scrollPos > itemPos.top) {
+            $(".app-header").css({
+                "position": "fixed",
+                "top": "0",
+                "z-index" : "2000"
+            });
+            $(".app-subheader").css({
+                "position" : "fixed",
+                "top" : headHeight,
+                "z-index" : "2000",
+                "box-shadow" : "0 1px 5px rgba(0, 0, 0, 0.5)"
+            });
+        } else {
+            console.log("top");
+            $(".app-header").css({
+                "position" : "static",
+                "z-index" : "1"
+            });
+            $(".app-subheader").css({
+                "position" : "static",
+                "z-index" : "1",
+                "box-shadow" : "none"
+            });
+        }
+        //console.log(scrollPos);
+    });
+
 });
