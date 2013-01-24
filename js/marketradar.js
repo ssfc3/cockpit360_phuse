76a382e0d5733fbcbec371c060a3b1a5e1c2945d
Tessa Thornton <tessathornton@gmail.com>
2013-01-24 12:00:51 -0500

    move scrolling js to separate file

---
+++ js/marketradar.js
@@ -0,0 +1,31 @@
+var itemPos = $(".app-header").offset();
+var headHeight = $(".app-header").css("height");
+
+$(document).scroll(function(){
+    var scrollPos = $(document).scrollTop(); 
+    if (scrollPos > itemPos.top) {
+        $(".app-header").css({
+            "position": "fixed",
+            "top": "0",
+            "z-index" : "5"
+        });
+        $(".app-subheader").css({
+            "position" : "fixed",
+            "top" : headHeight,
+            "z-index" : "5",
+            "box-shadow" : "0 1px 5px rgba(0, 0, 0, 0.5)"
+        });
+    } else {
+        console.log("top");
+        $(".app-header").css({
+            "position" : "static",
+            "z-index" : "1"
+        });
+        $(".app-subheader").css({
+            "position" : "static",
+            "z-index" : "1",
+            "box-shadow" : "none"
+        });
+    }
+    //console.log(scrollPos);
+});
