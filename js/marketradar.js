1ca049938e12746c18dee67ce4c6364fe1a01702
Aubrey Rhodes <aubrey.c.rhodes@gmail.com>
2013-02-12 12:55:56 -0500

    Fix scrolling for data page

--- js/marketradar.js
+++ js/marketradar.js
@@ -1,31 +1,35 @@
-var itemPos = $(".app-header").offset();
-var headHeight = $(".app-header").css("height");
+$(function() {
+  var itemPos = $(".app-header.scroll").offset();
+  var headHeight = $(".app-header").css("height");
 
-$(document).scroll(function(){
-    var scrollPos = $(document).scrollTop(); 
-    if (scrollPos > itemPos.top) {
-        $(".app-header").css({
-            "position": "fixed",
-            "top": "0",
-            "z-index" : "5"
-        });
-        $(".app-subheader").css({
-            "position" : "fixed",
-            "top" : headHeight,
-            "z-index" : "5",
-            "box-shadow" : "0 1px 5px rgba(0, 0, 0, 0.5)"
-        });
-    } else {
-        console.log("top");
-        $(".app-header").css({
-            "position" : "static",
-            "z-index" : "1"
-        });
-        $(".app-subheader").css({
-            "position" : "static",
-            "z-index" : "1",
-            "box-shadow" : "none"
-        });
-    }
-    //console.log(scrollPos);
+  if( itemPos && headHeight){
+    $(document).scroll(function(){
+        var scrollPos = $(document).scrollTop(); 
+        if (scrollPos > itemPos.top) {
+            $(".app-header").css({
+                "position": "fixed",
+                "top": "0",
+                "z-index" : "5"
+            });
+            $(".app-subheader").css({
+                "position" : "fixed",
+                "top" : headHeight,
+                "z-index" : "5",
+                "box-shadow" : "0 1px 5px rgba(0, 0, 0, 0.5)"
+            });
+        } else {
+            console.log("top");
+            $(".app-header").css({
+                "position" : "static",
+                "z-index" : "1"
+            });
+            $(".app-subheader").css({
+                "position" : "static",
+                "z-index" : "1",
+                "box-shadow" : "none"
+            });
+        }
+        //console.log(scrollPos);
+    });
+  }
 });
